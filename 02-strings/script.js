abab490c32c3cd900563109c912eb935f3f35687
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-02-24 10:57:14 +0100

    feat: fix exercises

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -14,18 +14,18 @@ const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // NON MODIFICARE QUESTA RIGA
 const firstName = 'Jane'; // NON MODIFICARE QUESTA RIGA
 const lastName = 'Smith'; // NON MODIFICARE QUESTA RIGA
 
-const fullName = "Jane Smith";
-console.log(firstName +' '+ lastName); // Output: "Jane Smith"
+const fullName = firstName +' '+ lastName;
+console.log(fullName); // Output: "Jane Smith"
 
 // 4. Imposta questa variabile utilizzando una proprietà
 // sulla variabile alphabet per ottenere la sua lunghezza
-const numberOfLettersInAlphabet = 26;
-console.log(alphabet.length); // Output: 26
+const numberOfLettersInAlphabet =alphabet.length;
+console.log(numberOfLettersInAlphabet); // Output: 26
 
 1.
 
-const name = "Valerio";
-console.log(name);
+const myName = "Valerio";
+console.log(myName);
 
 2.
 
